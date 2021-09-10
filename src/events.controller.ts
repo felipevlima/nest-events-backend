a2a0ff570d85c4ae0f44233c8d4d87cd29a2e1b0
Felipe Lima <felipe.lima@agilize.com.br>
2021-09-10 17:00:36 -0300

    MASTER: Initial commit

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,29 +1,50 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateEventDto } from './create-event.dto';
+import { UpdateEventDto } from './update-event.dto';
 
 @Controller('/events')
 export class EventsController {
   @Get()
   findAll() {
-    return;
+    return [
+      { id: 1, name: 'First event' },
+      { id: 2, name: 'Second event' },
+    ];
   }
 
-  @Get()
-  findOne() {
-    return;
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return { id: 1, name: 'First event' };
   }
 
   @Post()
-  create() {
-    return;
+  create(@Body() input: CreateEventDto) {
+    return input;
   }
 
-  @Put()
-  update() {
-    return;
+  @Put(':id')
+  update(@Param('id') id, @Body() input: UpdateEventDto) {
+    return {
+      item: id,
+      change: input,
+    };
   }
 
-  @Delete()
-  remove() {
-    return;
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id, @Body() input) {
+    return {
+      item: id,
+      change: input,
+    };
   }
 }
---
+++ src/update-event.dto.ts
@@ -0,0 +1,11 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateEventDto } from './create-event.dto';
+
+export class UpdateEventDto extends PartialType(CreateEventDto) {}
+
+// export class UpdateEventDto {
+//   name?: string;
+//   description?: string;
+//   when?: string;
+//   address?: string;
+// }
